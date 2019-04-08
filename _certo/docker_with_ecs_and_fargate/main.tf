bdafe3651a0b121750242116e5193a35209b22fc
Flávio Leandro <flavioleandro17@gmail.com>
2019-04-08 15:22:37 -0300

    Ajustado main.tf

--- _certo/docker_with_ecs_and_fargate/main.tf
+++ _certo/docker_with_ecs_and_fargate/main.tf
@@ -23,7 +23,7 @@ provider "aws" {
 # Fetch AZs in the current region
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "app" {
+resource "aws_vpc" "main" {
   cidr_block = "172.17.0.0/16"
 }
 
